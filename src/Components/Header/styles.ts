170c03335ebde0560e5e9fcb952baf42c00ab6a6
Alexandre Hideki Siroma <alexandresiroma@hotmail.com>
2023-01-02 22:30:10 -0300

    wip(Header): Create header styles

---
+++ src/Components/Header/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    h1: {},
+    h2: {},
+    h3: {},
+    h4: {},
+    h5: {},
+    h6: {},
+});
+
+export default styles;
