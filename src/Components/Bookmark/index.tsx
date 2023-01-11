97733ec73884fc14ac4d81af0bc382973294716d
Alexandre Hideki Siroma <alexandresiroma@hotmail.com>
2023-01-11 00:05:28 -0300

    feat(WebView): Added WebView in Bookmark

--- src/Components/Bookmark/index.tsx
+++ src/Components/Bookmark/index.tsx
@@ -1,57 +1,72 @@
 import { TouchableOpacity, View, Text, Image } from 'react-native';
-import { memo } from 'react';
+import { memo, useCallback, useState } from 'react';
 
 import { styles } from './styles';
 import { BookmarkProps } from './types';
+import { WebViewWithControl } from '../WebViewWithControl';
 
 const Bookmark = ({ data: { link, meta } }: BookmarkProps) => {
+  const [ isWebViewOpen, setIsWebViewOpen ] = useState(false);
+
+  const toggleWebViewIsOpen = useCallback(() => {
+    setIsWebViewOpen(prevState => !prevState);
+  }, []);
+
   return (
-    <TouchableOpacity
-      accessible
-      accessibilityRole='link'
-      accessibilityLabel="Bookmark"
-      accessibilityHint="Clique para abrir o link"
-      activeOpacity={0.2}
-      style={styles.wrapper}
-      onPress={() => console.log('Open web view')}
-    >
-      <View style={styles.container}>
-        <View style={styles.dataContainer}>
-          { (meta.title || meta.site_name) && (
-            <Text
-              style={styles.title}
-              numberOfLines={1}
-              ellipsizeMode='tail'
-            >
-              {meta.title || meta.site_name }
-            </Text>
-          )}
+    <>
+      <WebViewWithControl
+        uri={link}
+        isOpenWebView={isWebViewOpen}
+        toggleWebViewIsOpen={toggleWebViewIsOpen}
+      />
 
-          { meta.description && (
-            <Text style={styles.description} numberOfLines={2} ellipsizeMode='tail'>
-              {meta.description}
-            </Text>
-          )}
+      <TouchableOpacity
+        accessible
+        accessibilityRole='link'
+        accessibilityLabel="Bookmark"
+        accessibilityHint="Clique para abrir o link"
+        activeOpacity={0.2}
+        style={styles.wrapper}
+        onPress={() => console.log('Open web view')}
+      >
+        <View style={styles.container}>
+          <View style={styles.dataContainer}>
+            { (meta.title || meta.site_name) && (
+              <Text
+                style={styles.title}
+                numberOfLines={1}
+                ellipsizeMode='tail'
+              >
+                {meta.title || meta.site_name }
+              </Text>
+            )}
 
-          { link && (
-            <Text style={styles.link} numberOfLines={1} ellipsizeMode='tail'>
-              {link}
-            </Text>
-          )}
+            { meta.description && (
+              <Text style={styles.description} numberOfLines={2} ellipsizeMode='tail'>
+                {meta.description}
+              </Text>
+            )}
+
+            { link && (
+              <Text style={styles.link} numberOfLines={1} ellipsizeMode='tail'>
+                {link}
+              </Text>
+            )}
+          </View>
+
+          <View style={styles.imageContainer}>
+            { meta?.image?.url && (
+              <Image
+                source={{
+                  uri: meta.image.url,
+                }}
+                style={styles.image}
+              />
+            )}
+          </View>
         </View>
-
-        <View style={styles.imageContainer}>
-          { meta?.image?.url && (
-            <Image
-              source={{
-                uri: meta.image.url,
-              }}
-              style={styles.image}
-            />
-          )}
-        </View>
-      </View>
-    </TouchableOpacity>
+      </TouchableOpacity>
+    </>
   );
 };
 
---
+++ src/Components/WebViewWithControl/types.ts
@@ -0,0 +1,9 @@
+import { WebViewProps } from 'react-native-webview';
+
+export type WebViewWithControlProps = {
+  uri: string;
+  isOpenWebView: boolean;
+  toggleWebViewIsOpen: () => void
+} & WebViewProps;
+
+export default WebViewWithControlProps;
