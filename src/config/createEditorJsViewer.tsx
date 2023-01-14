c7a1382de9c1715fcbd95e7f9983afc3f6a1062d
Alexandre Hideki Siroma <alexandresiroma@hotmail.com>
2023-01-14 15:13:19 -0300

    feat(CraeadtCustomEditorJs): Removed disabled and some small updates

--- src/config/createEditorJsViewer.tsx
+++ src/config/createEditorJsViewer.tsx
@@ -2,7 +2,6 @@
 import { memo } from 'react';
 import { View } from 'react-native';
 
-import { HandleCustomToolComponent } from '../components/HandleCustomToolComponent';
 import { FallbackBlock } from '../components/FallbackBlock';
 import { Paragraph } from '../components/Paragraph';
 import { Header } from '../components/Header';
@@ -10,134 +9,147 @@ import { List } from '../components/List';
 import { SimpleImage } from '../components/SimpleImage';
 import { ImageFrame } from '../components/ImageFrame';
 import { Bookmark } from '../components/Bookmark';
+import { Quote } from '../components/Quote';
 
 import { EditorJsViwerNativeProps } from '../types';
-import { CreateEditorJsViewerProps } from '../types/createEditorJsViewerProps';
+import { ICreateEditorJsViewerProps } from '../types/createEditorJsViewerProps';
 
 export const createEditorJsViewer = ({
-  toolsParser: {
+  toolsParser,
+  unknownBlockFallback = true
+}: ICreateEditorJsViewerProps) => {
+  const {
     header,
     image,
     linkTool,
     list,
     paragraph,
-    simpleImage
-  }
-}: CreateEditorJsViewerProps) => {
-  return memo(({ data, ...rest }: EditorJsViwerNativeProps) => {
-    return (
-      <View style={{ width: '100%' }} {...rest}>
-        {data.blocks.map((block) => {
-          if (block.type == 'image' && block.data?.file == null) {
-            block.type = 'simpleImage';
+    simpleImage,
+    quote,
+  } = toolsParser ?? {};
+
+  return memo(({ data, ...rest }: EditorJsViwerNativeProps) => (
+    <View style={{ width: '100%' }} {...rest}>
+      {data.blocks.map((block) => {
+        if (block.type == 'image' && block.data?.file == null) {
+          block.type = 'simpleImage';
+        }
+
+        switch (block.type) {
+          case 'header': {
+            const CustomHeader = header?.CustomComponent;
+
+            return (
+              <>
+                {CustomHeader ? (
+                  <CustomHeader key={block.id} data={block.data} />
+                ) : (
+                  <Header key={block.id} data={block.data} />
+                )}
+              </>
+            );
           }
 
-          switch (block.type) {
-            case 'header': {
-              const CustomHeader = header?.CustomComponent;
-              return (
-                <HandleCustomToolComponent
-                  block={block}
-                  disabled={header?.disabled}
-                  defaultComponent={
-                    <Header key={block.id} data={block.data} />
-                  }
-                  customComponent={
-                    CustomHeader && <CustomHeader key={block.id} data={block.data} />
-                  }
-                />
-              );
-            }
+          case 'image': {
+            const CustomImage = image?.CustomComponent;
 
-            case 'image': {
-              const CustomImage = image?.CustomComponent;
-              return (
-                <HandleCustomToolComponent
-                  block={block}
-                  disabled={image?.disabled}
-                  defaultComponent={
-                    <ImageFrame key={block.id} data={block.data} />
-                  }
-                  customComponent={
-                    CustomImage && <CustomImage key={block.id} data={block.data} />
-                  }
-                />
-              );
-            }
-
-            case 'linkTool': {
-              const CustomLinkTool = linkTool?.CustomComponent;
-              return (
-                <HandleCustomToolComponent
-                  block={block}
-                  disabled={linkTool?.disabled}
-                  defaultComponent={
-                    <Bookmark key={block.id} data={block.data} />
-                  }
-                  customComponent={
-                    CustomLinkTool && <CustomLinkTool key={block.id} data={block.data} />
-                  }
-                />
-              );
-            }
-
-            case 'list': {
-              const CustomList = list?.CustomComponent;
-              return (
-                <HandleCustomToolComponent
-                  block={block}
-                  disabled={list?.disabled}
-                  defaultComponent={
-                    <List key={block.id} data={block.data} />
-                  }
-                  customComponent={
-                    CustomList && <CustomList key={block.id} data={block.data} />
-                  }
-                />
-              );
-            }
-
-            case 'paragraph': {
-              const CustomParagraph = paragraph?.CustomComponent;
-              return (
-                <HandleCustomToolComponent
-                  block={block}
-                  disabled={paragraph?.disabled}
-                  defaultComponent={
-                    <Paragraph key={block.id} data={block.data} />
-                  }
-                  customComponent={
-                    CustomParagraph && <CustomParagraph key={block.id} data={block.data} />
-                  }
-                />
-              );
-            }
-
-            case 'simpleImage': {
-              const CustomSimpleImage = simpleImage?.CustomComponent;
-              return (
-                <HandleCustomToolComponent
-                  block={block}
-                  disabled={simpleImage?.disabled}
-                  defaultComponent={
-                    <SimpleImage key={block.id} data={block.data} />
-                  }
-                  customComponent={
-                    CustomSimpleImage && <CustomSimpleImage key={block.id} data={block.data} />
-                  }
-                />
-              );
-            }
-
-            case 'customBlockType': {
-              // TODO
-              return <></>;
-            }
-
-            default: return <FallbackBlock key={block.id} blockType={block.type} />;
+            return (
+              <>
+                {CustomImage ? (
+                  <CustomImage key={block.id} data={block.data} />
+                ) : (
+                  <ImageFrame key={block.id} data={block.data} />
+                )}
+              </>
+            );
           }
-        })}
-      </View>
-    );
-  });
+
+          case 'linkTool': {
+            const CustomLinkTool = linkTool?.CustomComponent;
+
+            return (
+              <>
+                {CustomLinkTool ? (
+                  <CustomLinkTool key={block.id} data={block.data} />
+                ) : (
+                  <Bookmark key={block.id} data={block.data} />
+                )}
+              </>
+            );
+          }
+
+          case 'list': {
+            const CustomList = list?.CustomComponent;
+
+            return (
+              <>
+                {CustomList ? (
+                  <CustomList key={block.id} data={block.data} />
+                ) : (
+                  <List key={block.id} data={block.data} />
+                )}
+              </>
+            );
+          }
+
+          case 'paragraph': {
+            const CustomParagraph = paragraph?.CustomComponent;
+
+            return (
+              <>
+                {CustomParagraph ? (
+                  <CustomParagraph key={block.id} data={block.data} />
+                ) : (
+                  <Paragraph key={block.id} data={block.data} />
+                )}
+              </>
+            );
+          }
+
+          case 'simpleImage': {
+            const CustomSimpleImage = simpleImage?.CustomComponent;
+
+            return (
+              <>
+                {CustomSimpleImage ? (
+                  <CustomSimpleImage key={block.id} data={block.data} />
+                ) : (
+                  <SimpleImage key={block.id} data={block.data} />
+                )}
+              </>
+            );
+          }
+
+          case 'quote': {
+            const CustomQuote = quote?.CustomComponent ?? Quote;
+
+            return (
+              <>
+                {CustomQuote ? (
+                  <CustomQuote key={block.id} data={block.data} />
+                ) : (
+                  <Quote key={block.id} data={block.data} />
+                )}
+              </>
+            );
+          }
+
+          // TODO
+          // case 'customBlockType': {
+          //   return <></>;
+          // }
+
+          default: return (
+            <>
+              {unknownBlockFallback ? (
+                <FallbackBlock key={block.id} blockType={block.type} />
+              ) : (
+                null
+              ) }
+            </>
+          );
+        }
+      })}
+    </View>
+  ));
 };
--- src/types/createEditorJsViewerProps.ts
+++ src/types/createEditorJsViewerProps.ts
@@ -4,57 +4,63 @@ import { BookmarkProps } from '../components/Bookmark/types';
 import { ListProps } from '../components/List/types';
 import { ParagraphProps } from '../components/Paragraph/types';
 import { SimpleImageProps } from '../components/SimpleImage/types';
+import { QuoteProps } from '../components/Quote/types';
 
-export type CreateEditorJsViewerProps = {
-  toolsParser: {
-    header?: {
-      /**
-       * A component with type {@link HeaderProps} or any for your custom header tool
-      */
-      CustomComponent?: (props: HeaderProps ) => JSX.Element
-      disabled?: boolean;
-    }
-    image?: {
-      /**
-       * A component with type {@link ImageFrameProps} or any for your custom image tool
-      */
-      CustomComponent: (props: ImageFrameProps | any ) => JSX.Element
-      disabled?: boolean;
-    }
-    linkTool?: {
-      /**
-       * A component with type {@link BookmarkProps} or any for your custom linkTool tool
-      */
-      CustomComponent?: (props: BookmarkProps | any ) => JSX.Element
-      disabled?: boolean;
-    }
-    /**
-     * Is not recommended to replace this component because he need a html parser to RN JSX
-    */
-    list?: {
-      /**
-       * A component with type {@link ListProps} or any for your custom list tool
-      */
-      CustomComponent?: (prop: ListProps | any ) => JSX.Element
-      disabled?: boolean;
-    }
-    /**
-     * Is not recommended to replace this component because he need a html parser to RN JSX
-    */
-    paragraph?: {
-      /**
-       * A component with type {@link ParagraphProps} or any for your custom paragraph tool
-      */
-      CustomComponent?: (props: ParagraphProps | any ) => JSX.Element
-      disabled?: boolean;
-    }
-    simpleImage?: {
-      /**
-       * A component with type {@link SimpleImageProps } or any for your custom simpleImage tool
-      */
-      CustomComponent?: (props: SimpleImageProps | any ) => JSX.Element
-      disabled?: boolean;
-    }
-  };
+export interface ICreateEditorJsViewerProps {
+  toolsParser?: IToolsParser;
+  /**
+   * Show a fallbac block when type block type is unknown. Default is `true`
+   */
+  unknownBlockFallback?: boolean;
 }
 
+export interface IToolsParser {
+  header?: {
+    /**
+     * A component with type {@link HeaderProps} or any for your custom header tool
+    */
+    CustomComponent?: (props: HeaderProps | any ) => JSX.Element
+  }
+  image?: {
+    /**
+     * A component with type {@link ImageFrameProps} or any for your custom image tool
+    */
+    CustomComponent: (props: ImageFrameProps | any ) => JSX.Element
+  }
+  linkTool?: {
+    /**
+     * A component with type {@link BookmarkProps} or any for your custom linkTool tool
+    */
+    CustomComponent?: (props: BookmarkProps | any ) => JSX.Element
+  }
+  /**
+   * Is not recommended to replace this component because he need a html parser to RN JSX
+  */
+  list?: {
+    /**
+     * A component with type {@link ListProps} or any for your custom list tool
+    */
+    CustomComponent?: (prop: ListProps | any ) => JSX.Element
+  }
+  /**
+   * Is not recommended to replace this component because he need a html parser to RN JSX
+  */
+  paragraph?: {
+    /**
+     * A component with type {@link ParagraphProps} or any for your custom paragraph tool
+    */
+    CustomComponent?: (props: ParagraphProps | any ) => JSX.Element
+  }
+  simpleImage?: {
+    /**
+     * A component with type {@link SimpleImageProps } or any for your custom simpleImage tool
+    */
+    CustomComponent?: (props: SimpleImageProps | any ) => JSX.Element
+  }
+  quote?: {
+    /**
+     * A component with type {@link QuoteProps } or any for your custom simpleImage tool
+    */
+    CustomComponent?: (props: QuoteProps | any ) => JSX.Element
+  }
+}
