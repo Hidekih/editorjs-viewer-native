da9ff05ba72d965921393c030eb07174f9767e5a
Alexandre Hideki Siroma <alexandresiroma@hotmail.com>
2023-06-10 12:41:07 -0300

    fix indent

--- src/config/createEditorJsViewer.tsx
+++ src/config/createEditorJsViewer.tsx
@@ -51,43 +51,43 @@ export const createEditorJsViewer = (props?: ICreateEditorJsViewerProps) => {
             const CustomHeader = delimiter?.CustomComponent;
 
             return CustomHeader ? (
-                <CustomHeader key={block.id} data={block.data} />
-              ) : (
-                <Delimiter
-                  key={block.id}
-                  data={block.data}
-                  style={fixMarginIfIsFirstOrLast}
-                />
-              );
+              <CustomHeader key={block.id} data={block.data} />
+            ) : (
+              <Delimiter
+                key={block.id}
+                data={block.data}
+                style={fixMarginIfIsFirstOrLast}
+              />
+            );
           }
 
           case 'header': {
             const CustomHeader = header?.CustomComponent;
 
             return CustomHeader ? (
-                <CustomHeader key={block.id} data={block.data} />
-              ) : (
-                <Header
-                  key={block.id}
-                  data={block.data}
-                  style={fixMarginIfIsFirstOrLast}
-                  fontFamily={header?.fontFamily}
-                />
-              );
+              <CustomHeader key={block.id} data={block.data} />
+            ) : (
+              <Header
+                key={block.id}
+                data={block.data}
+                style={fixMarginIfIsFirstOrLast}
+                fontFamily={header?.fontFamily}
+              />
+            );
           }
 
           case 'image': {
             const CustomImage = image?.CustomComponent;
 
             return CustomImage ? (
-                <CustomImage key={block.id} data={block.data} />
-              ) : (
-                <ImageFrame
-                  key={block.id}
-                  data={block.data}
-                  captionFontFamily={image?.captionFontFamily}
-                  style={fixMarginIfIsFirstOrLast}
-                />
+              <CustomImage key={block.id} data={block.data} />
+            ) : (
+              <ImageFrame
+                key={block.id}
+                data={block.data}
+                captionFontFamily={image?.captionFontFamily}
+                style={fixMarginIfIsFirstOrLast}
+              />
             );
           }
 
@@ -95,89 +95,89 @@ export const createEditorJsViewer = (props?: ICreateEditorJsViewerProps) => {
             const CustomLinkTool = linkTool?.CustomComponent;
 
             return CustomLinkTool ? (
-                  <CustomLinkTool key={block.id} data={block.data} />
-                ) : (
-                  <LinkTool
-                    key={block.id}
-                    data={block.data}
-                    style={fixMarginIfIsFirstOrLast}
-                  />
-                );
+              <CustomLinkTool key={block.id} data={block.data} />
+            ) : (
+              <LinkTool
+                key={block.id}
+                data={block.data}
+                style={fixMarginIfIsFirstOrLast}
+              />
+            );
           }
 
           case 'list': {
             const CustomList = list?.CustomComponent;
 
             return CustomList ? (
-                  <CustomList key={block.id} data={block.data} />
-                ) : (
-                  <List
-                    key={block.id}
-                    data={block.data}
-                    fontFamily={list?.fontFamily}
-                    style={fixMarginIfIsFirstOrLast}
-                  />
-                );
+              <CustomList key={block.id} data={block.data} />
+            ) : (
+              <List
+                key={block.id}
+                data={block.data}
+                fontFamily={list?.fontFamily}
+                style={fixMarginIfIsFirstOrLast}
+              />
+            );
           }
 
           case 'paragraph': {
             const CustomParagraph = paragraph?.CustomComponent;
 
             return CustomParagraph ? (
-                  <CustomParagraph key={block.id} data={block.data} />
-                ) : (
-                  <Paragraph
-                    key={block.id}
-                    data={block.data}
-                    fontFamily={paragraph?.fontFamily}
-                    style={fixMarginIfIsFirstOrLast}
-                  />
-                );
+              <CustomParagraph key={block.id} data={block.data} />
+            ) : (
+              <Paragraph
+                key={block.id}
+                data={block.data}
+                fontFamily={paragraph?.fontFamily}
+                style={fixMarginIfIsFirstOrLast}
+              />
+            );
           }
 
           case 'personality': {
             const CustomPersonality = personality?.CustomComponent;
 
             return CustomPersonality ? (
-                  <CustomPersonality key={block.id} data={block.data} />
-                ) : (
-                  <Personality
-                    key={block.id}
-                    data={block.data}
-                    style={fixMarginIfIsFirstOrLast}
-                  />
-                );
+              <CustomPersonality key={block.id} data={block.data} />
+            ) : (
+              <Personality
+                key={block.id}
+                data={block.data}
+                style={fixMarginIfIsFirstOrLast}
+              />
+            );
           }
 
           case 'simpleImage': {
             const CustomSimpleImage = simpleImage?.CustomComponent;
 
             return CustomSimpleImage ? (
-                  <CustomSimpleImage key={block.id} data={block.data} />
-                ) : (
-                  <SimpleImage
-                    key={block.id}
-                    data={block.data}
-                    captionFontFamily={simpleImage?.captionFontFamily}
-                    style={fixMarginIfIsFirstOrLast}
-                  />
-                );
+              <CustomSimpleImage key={block.id} data={block.data} />
+            ) : (
+              <SimpleImage
+                key={block.id}
+                data={block.data}
+                captionFontFamily={simpleImage?.captionFontFamily}
+                style={fixMarginIfIsFirstOrLast}
+              />
+            );
           }
 
           case 'quote': {
             const CustomQuote = quote?.CustomComponent ?? Quote;
 
             return CustomQuote ? (
-                  <CustomQuote key={block.id} data={block.data} />
-                ) : (
-                  <Quote
-                    key={block.id}
-                    data={block.data}
-                    quoteFontFamily={quote?.quoteFontFamily}
-                    captionFontFamily={quote?.captionFontFamily}
-                    style={fixMarginIfIsFirstOrLast}
-                  />
-                );
+              <CustomQuote key={block.id} data={block.data} />
+            ) : (
+              <Quote
+                key={block.id}
+                data={block.data}
+                quoteFontFamily={quote?.quoteFontFamily}
+                captionFontFamily={quote?.captionFontFamily}
+                style={fixMarginIfIsFirstOrLast}
+              />
+            );
           }
 
           // TODO
@@ -186,10 +186,10 @@ export const createEditorJsViewer = (props?: ICreateEditorJsViewerProps) => {
           // }
 
           default: return props?.unknownBlockFallback ? (
-                <FallbackBlock key={block.id} blockType={block.type} />
-            ) : (
-              null
-            );
+            <FallbackBlock key={block.id} blockType={block.type} />
+          ) : (
+            null
+          );
         }
       })}
     </View>
