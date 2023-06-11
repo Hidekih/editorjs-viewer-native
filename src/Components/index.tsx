541ce89c714749485938e3394d1668134a3beecb
Alexandre Hideki Siroma <alexandresiroma@hotmail.com>
2023-06-11 01:11:34 -0300

    chore: Simplify index.ts export file

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -1,30 +1,44 @@
 export { Bold } from './Bold';
 export type { IBoldProps } from './Bold/types';
+
 export { Code } from './Code';
 export type { ICodeProps } from './Code/types';
+
 export { Delimiter } from './Delimiter';
 export type { IDelimiterProps } from './Delimiter/types';
+
 export { FallbackBlock } from './FallbackBlock';
 export type { IFallbackBlockProps } from './FallbackBlock/types';
+
 export { Header } from './Header';
 export type { IHeaderProps } from './Header/types';
+
 export { ImageFrame } from './ImageFrame';
 export type { IImageFrameProps } from './ImageFrame/types';
+
 export { Italic } from './Italic';
 export type { IItalicProps } from './Italic/types';
+
 export { LinkTool } from './LinkTool';
 export type { ILinkToolProps } from './LinkTool/types';
+
 export { List } from './List';
 export type { IListProps } from './List/types';
+
 export { Mark } from './Mark';
 export type { IMarkProps } from './Mark/types';
+
 export { Paragraph } from './Paragraph';
 export type { IParagraphProps } from './Paragraph/types';
+
 export { Personality } from './Personality';
 export type { IPersonalityProps } from './Personality/types';
+
 export { Quote } from './Quote';
 export type { IQuoteProps } from './Quote/types';
+
 export { SimpleImage } from './SimpleImage';
 export type { ISimpleImageProps } from './SimpleImage/types';
+
 export { Underline } from './Underline';
 export type { IUnderlineProps } from './Underline/types';
